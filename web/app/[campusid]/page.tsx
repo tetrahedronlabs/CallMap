3deb3d1241770adc80e3695082eeacc755af9b29
brandnholl <brandnlholl@gmail.com>
2024-03-30 11:57:20 -0700

    Added Avatar and Avatar Group Components

--- web/app/[campusid]/page.tsx
+++ web/app/[campusid]/page.tsx
@@ -1,17 +1,23 @@
 import { supabase } from "@/lib/supabase/client";
 import { notFound } from "next/navigation";
 
+// eslint-disable-next-line @next/next/no-async-client-component
 export default async function Campus({
-    params: { campusid },
-  }: {
-    params: { campusid: string };
-  }) {
-    const { data: campus } = await supabase
-      .from("campuses")
-      .select()
-      .match({ campus_id: campusid })
-      .single();
-    if (!campus) {
-      notFound();
-    }
-    return (<div>{campus.name}</div>)};
+  params: { campusid },
+}: {
+  params: { campusid: string };
+}) {
+  const { data: campus } = await supabase
+    .from("campuses")
+    .select()
+    .match({ campus_id: campusid })
+    .single();
+  if (!campus) {
+    notFound();
+  }
+  return (
+    <div>
+{campus.name}
+    </div>
+  );
+}
