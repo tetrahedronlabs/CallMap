197b202a8ec88090d1c2c779fc5823308dbce3af
brandnholl <brandnlholl@gmail.com>
2024-03-31 15:50:04 -0700

    Added Manifest

---
+++ web/app/manifest.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "CallMap App",
+    short_name: "CallMap",
+    description: "CallMap – the open-source platform for campus safety data.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    //TODO: Add icons
+  };
+}3
