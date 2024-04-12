b9f4988e805b08fffe56eb15770859ab61b6128f
Brandon <brandnlholl@gmail.com>
2024-04-11 17:02:06 -0700

    fix formatting

--- apps/web/lib/record.ts
+++ apps/web/lib/record.ts
@@ -3,13 +3,13 @@ import { supabase } from "./supabase/client";
 export async function getRecord(
   campusid: string,
   locationid: string,
-  recordid: string
+  recordid: string,
 ) {
   const { data: record } = await supabase
     .from("records")
     .select()
     .match({ campus: campusid })
-    .match({parsed_location: locationid})
+    .match({ parsed_location: locationid })
     .match({ record_id: recordid })
     .single();
   if (!record) {
