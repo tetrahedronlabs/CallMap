9779d455ee65e60897be5d23a3060d98c08acfbd
Brandon <brandnlholl@gmail.com>
2024-04-23 11:54:08 -0700

    Added Astro Docs

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'CallMap',
+			social: {
+				github: 'https://github.com/tetrahedronlabs/CallMap',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
