1de6f9c87421c35bf4547bbb4fbf20a5a1620b5c
Brandon <brandnlholl@gmail.com>
2024-05-12 19:34:22 -0700

    Added analytics tracking

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import type { Metadata } from 'next';
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: { template: '%s | CallMap', default: 'CallMap' },
@@ -16,6 +17,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<Script
+				strategy="lazyOnload"
+				src="https://analytics.tetrahedron.dev/script.js"
+				data-website-id="25fe5d84-b10b-4445-8d61-e048f30d4569"
+			/>
 			<body className={`${GeistSans.className} ${GeistMono.variable}`}>
 				{children}
 				<Toaster />
